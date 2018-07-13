a8b96c99faa78f56410e1a4810ac83b1614749a2
TP034850 <TP034850@mail.apu.edu.my>
2018-07-13 20:03:40 +0800

    Add project files.

---
+++ DDAC_Assignment/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DDAC_Assignment.Startup))]
+namespace DDAC_Assignment
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
