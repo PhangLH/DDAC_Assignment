299264b4b6331633b4d3c68dd348122fa68fddac
TP034850 <TP034850@mail.apu.edu.my>
2018-07-23 22:06:27 +0800

    Fixed change password

--- DDAC_Assignment/ChangePassword.aspx.cs
+++ DDAC_Assignment/ChangePassword.aspx.cs
@@ -19,30 +19,59 @@ namespace DDAC_Assignment
 
         protected void ChangePassword_Click(object sender, EventArgs e)
         {
+            var oldPassword = "";
             int currentId = int.Parse(Session["UserId"].ToString());
 
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
 
-            string changePassword = "UPDATE Users SET use_password = @password WHERE use_id = @id;";
-            SqlCommand cmdChangePassword = new SqlCommand(changePassword, conn);
-            cmdChangePassword.Parameters.Add("@password", SqlDbType.NVarChar);
-            cmdChangePassword.Parameters["@password"].Value = ConfirmPassword.Text;
-            cmdChangePassword.Parameters.Add("@id", SqlDbType.Int);
-            cmdChangePassword.Parameters["@id"].Value = currentId;
-
+            //check old and new password
+            string checkChangePassword = "SELECT use_password FROM Users WHERE use_id = @id;";
+            SqlCommand cmdCheckChangePassword = new SqlCommand(checkChangePassword, conn);
+            cmdCheckChangePassword.Parameters.Add("@id", SqlDbType.Int);
+            cmdCheckChangePassword.Parameters["@id"].Value = currentId;
             conn.Open();
-            int success = cmdChangePassword.ExecuteNonQuery();
+            SqlDataReader dr = cmdCheckChangePassword.ExecuteReader();
+            if (dr.Read())
+            {
+                oldPassword = dr["use_password"].ToString();
+                dr.Close();
+            }
             conn.Close();
 
-            //not success
-            if (success == 0)
+            if (oldPassword.Equals(ConfirmPassword.Text))
             {
+                //error message
+                Type cstype = this.GetType();
+                ClientScriptManager cs = Page.ClientScript;
+                if (!cs.IsStartupScriptRegistered(cstype, "PopupScript"))
+                {
+                    String cstext = "alert('Invalid Change! Make sure new password is different with old password');";
+                    cs.RegisterStartupScript(cstype, "PopupScript", cstext, true);
+                }
             }
-            //success
             else
             {
-                Response.Redirect("/Account/Login", false);
+                string changePassword = "UPDATE Users SET use_password = @password WHERE use_id = @id;";
+                SqlCommand cmdChangePassword = new SqlCommand(changePassword, conn);
+                cmdChangePassword.Parameters.Add("@password", SqlDbType.NVarChar);
+                cmdChangePassword.Parameters["@password"].Value = ConfirmPassword.Text;
+                cmdChangePassword.Parameters.Add("@id", SqlDbType.Int);
+                cmdChangePassword.Parameters["@id"].Value = currentId;
+
+                conn.Open();
+                int success = cmdChangePassword.ExecuteNonQuery();
+                conn.Close();
+
+                //not success
+                if (success == 0)
+                {
+                }
+                //success
+                else
+                {
+                    Response.Redirect("/Account/Login", false);
+                }
             }
         }
     }
